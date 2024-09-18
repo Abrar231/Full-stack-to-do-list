7c31198bfc3bbb7b79bf7835576d1dc878a77deb
Abrar <abrarmadki1@gmail.com>
2024-09-19 01:24:50 +0530

    Backend folder Init and Setting up database using Sequelize-CLI

---
+++ backend/migrations/20240917210348-create-tasks.js
@@ -0,0 +1,27 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      task: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
